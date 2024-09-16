fa0dd02f1e4dc6e540b5b3cdce1d5742de353edd
ItzDivyaaa <divyakartik2009@gmail.com>
2024-09-16 17:58:19 +0530

    Added routes in sidenav

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+'use client';
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { Layout, PiggyBank, ReceiptText, ShieldCheck } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
 
 function SideNav() {
     const menuList=[
         {
             id:1,
             name:'Dashboard',
-            icon:Layout
+            icon:Layout,
+            path:'/dashboard'
         },
         {
             id:2,
             name:'Budgets',
-            icon:PiggyBank
+            icon:PiggyBank,
+            path:'/dashboard/budgets'
         },{
             id:3,
             name:'Expenses',
-            icon:ReceiptText
+            icon:ReceiptText,
+            path:'/dashboard/expenses'
         },{
             id:4,
             name:'Upgrade',
-            icon:ShieldCheck
+            icon:ShieldCheck,
+            path:'/dashboard/upgrade'
         }
     ]
+    const path=usePathname();
+
+    useEffect(()=>{
+        console.log(path)
+    },[])
   return (
 
     <div className='h-screen p-5 border shadow-md'>
